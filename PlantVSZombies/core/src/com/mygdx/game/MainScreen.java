bebfa797713a25830ac784edc9f2de8eb7525fed
Mohammed Alaa <zicoreal@ymail.com>
2020-03-25 23:29:35 +0200

    adding only one zombie

--- PlantVSZombies/core/src/com/mygdx/game/MainScreen.java
+++ PlantVSZombies/core/src/com/mygdx/game/MainScreen.java
@@ -1,4 +1,5 @@
 package com.mygdx.game;
+
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -16,60 +17,73 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
-public class MainScreen implements Screen 
-{
+
+public class MainScreen implements Screen {
     private Texture sunflower;
     private Texture peashooter;
+    private Texture zombietexture;
     private MyGdxGame game;
     private float elapsed;
     private Character p;
     private Character plant;
-   	private final int coloumnPosition[]= {56,171,290,430,545 },rowPosition[]= {400,500,596,693,781,880,972,1067,1161};
+    private Character zombiechar;
 
-    public MainScreen(MyGdxGame game)
-    {
-        this.game=game;
-        p=new Character("peaSpriteSheet.png",4,6,320,250,0.1f);
-        plant=new Character("sunflowersheet.png",9,6,320,350,0.05f);
-        sunflower=new Texture("sunflower.png");
-        peashooter=new Texture("PeashooterCard.png");
-        elapsed=0;
+    private final int coloumnPosition[] = {56, 171, 290, 430, 545}, rowPosition[] = {400, 500, 596, 693, 781, 880, 972, 1067, 1161};
+
+    public MainScreen(MyGdxGame game) {
+        this.game = game;
+        p = new Character("peaSpriteSheet.png", 4, 6, 320, 250, 0.1f);
+        plant = new Character("sunflowersheet.png", 9, 6, 320, 350, 0.05f);
+        zombiechar = new Character("ConeZombie2.png", 10, 5, 1200, 300, 0.05f);
+        sunflower = new Texture("sunflower.png");
+        peashooter = new Texture("PeashooterCard.png");
+        zombietexture = new Texture("ConeZombie2.png");
+        elapsed = 0;
     }
+
     @Override
-    public void show(){
-        
+    public void show() {
+
     }
+
     @Override
-    public void render(float delta){
-       elapsed+=delta;
-       Gdx.gl.glClearColor(1, 0, 0, 1);
-       Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-       game.batch.begin();
-       game.batch.draw(game.img,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-       game.batch.draw(sunflower,30,600);
-       game.batch.draw(peashooter,30,500,sunflower.getWidth(),sunflower.getHeight());
-       game.batch.draw((TextureRegion)p.DrawPlant().getKeyFrame(elapsed,true),p.Getx(),p.Gety());
-       game.batch.draw((TextureRegion)plant.DrawPlant().getKeyFrame(elapsed,true),plant.Getx(),plant.Gety());
-       game.batch.end();
+    public void render(float delta) {
+        elapsed += delta;
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        zombiechar.update(zombiechar.Getx()-0.5f,zombiechar.Gety());
+        game.batch.draw(game.img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        game.batch.draw(sunflower, 30, 600);
+        game.batch.draw(peashooter, 30, 500, sunflower.getWidth(), sunflower.getHeight());
+        game.batch.draw((TextureRegion) p.DrawPlant().getKeyFrame(elapsed, true), p.Getx(), p.Gety());
+        game.batch.draw((TextureRegion) plant.DrawPlant().getKeyFrame(elapsed, true), plant.Getx(), plant.Gety());
+        game.batch.draw((TextureRegion) zombiechar.DrawPlant().getKeyFrame(elapsed, true), zombiechar.Getx(), zombiechar.Gety());
+        game.batch.end();
     }
+
     @Override
     public void resize(int width, int height) {
-        
+
     }
+
     @Override
     public void pause() {
-        
+
     }
+
     @Override
     public void resume() {
-       
+
     }
+
     @Override
     public void hide() {
-      
+
     }
+
     @Override
     public void dispose() {
-       
+
     }
 }
